6b1155f136c0f3546fff2b5a8d4911338c342630
menuz <dmnrei@gmail.com>
2014-01-09 20:25:23 +0800

    init kdhb

---
+++ kdhb/src/com/kdhb/Main.java
@@ -0,0 +1,22 @@
+package com.kdhb;
+
+/**
+ * 
+ * TODO
+ * @author: dmnrei@gmail.com 
+ * @version: V1.0
+ */
+public class Main {
+
+	/** 
+	 * TODO
+	 *  
+	 * @param args void
+	 */
+	public static void main(String[] args) {
+		System.out.println("Make kdhb a success!!");
+	}
+
+}
+
+
